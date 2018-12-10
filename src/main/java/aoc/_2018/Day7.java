6cbc7b125a1bbc91aea367caad8050da1a0450e2
Dan Fielding <dgfielding@gmail.com>
2018-12-10 12:03:45 +0000

    2018/Day7, 8 and 9 solutions

---
+++ src/main/java/aoc/_2018/Day7.java
@@ -0,0 +1,251 @@
+package aoc._2018;
+
+import com.google.common.collect.ImmutableList;
+import shared.ResourceUtil;
+import shared.Test;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Dan Fielding
+ */
+public class Day7 {
+
+/*
+--- Day 7: The Sum of Its Parts ---
+
+You find yourself standing on a snow-covered coastline; apparently, you landed a
+little off course. The region is too hilly to see the North Pole from here, but
+you do spot some Elves that seem to be trying to unpack something that washed
+ashore. It's quite cold out, so you decide to risk creating a paradox by asking
+them for directions.
+
+"Oh, are you the search party?" Somehow, you can understand whatever Elves from
+the year 1018 speak; you assume it's Ancient Nordic Elvish. Could the device on
+your wrist also be a translator? "Those clothes don't look very warm; take
+this." They hand you a heavy coat.
+
+"We do need to find our way back to the North Pole, but we have higher
+priorities at the moment. You see, believe it or not, this box contains
+something that will solve all of Santa's transportation problems - at least,
+that's what it looks like from the pictures in the instructions." It doesn't
+seem like they can read whatever language it's in, but you can: "Sleigh kit.
+Some assembly required."
+
+"'Sleigh'? What a wonderful name! You must help us assemble this 'sleigh' at
+once!" They start excitedly pulling more parts out of the box.
+
+The instructions specify a series of steps and requirements about which steps
+must be finished before others can begin (your puzzle input). Each step is
+designated by a single letter. For example, suppose you have the following
+instructions:
+
+Step C must be finished before step A can begin.
+Step C must be finished before step F can begin.
+Step A must be finished before step B can begin.
+Step A must be finished before step D can begin.
+Step B must be finished before step E can begin.
+Step D must be finished before step E can begin.
+Step F must be finished before step E can begin.
+
+Visually, these requirements look like this:
+
+
+  -->A--->B--
+ /    \      \
+C      -->D----->E
+ \           /
+  ---->F-----
+
+Your first goal is to determine the order in which the steps should be
+completed. If more than one step is ready, choose the step which is first
+alphabetically. In this example, the steps would be completed as follows:
+
+    Only C is available, and so it is done first.
+    Next, both A and F are available. A is first alphabetically, so it is done
+        next.
+    Then, even though F was available earlier, steps B and D are now also
+        available, and B is the first alphabetically of the three.
+    After that, only D and F are available. E is not available because only some
+        of its prerequisites are complete. Therefore, D is completed next.
+    F is the only choice, so it is done next.
+    Finally, E is completed.
+
+So, in this example, the correct order is CABDFE.
+
+In what order should the steps in your instructions be completed?
+
+--- Part Two ---
+
+As you're about to begin construction, four of the Elves offer to help. "The sun
+will set soon; it'll go faster if we work together." Now, you need to account
+for multiple people working on steps simultaneously. If multiple steps are
+available, workers should still begin them in alphabetical order.
+
+Each step takes 60 seconds plus an amount corresponding to its letter:
+A=1, B=2, C=3, and so on. So, step A takes 60+1=61 seconds, while step Z takes
+60+26=86 seconds. No time is required between steps.
+
+To simplify things for the example, however, suppose you only have help from one
+Elf (a total of two workers) and that each step takes 60 fewer seconds (so that
+step A takes 1 second and step Z takes 26 seconds). Then, using the same
+instructions as above, this is how each second would be spent:
+
+Second   Worker 1   Worker 2   Done
+   0        C          .
+   1        C          .
+   2        C          .
+   3        A          F       C
+   4        B          F       CA
+   5        B          F       CA
+   6        D          F       CAB
+   7        D          F       CAB
+   8        D          F       CAB
+   9        D          .       CABF
+  10        E          .       CABFD
+  11        E          .       CABFD
+  12        E          .       CABFD
+  13        E          .       CABFD
+  14        E          .       CABFD
+  15        .          .       CABFDE
+
+Each row represents one second of time. The Second column identifies how many
+seconds have passed as of the beginning of that second. Each worker column shows
+the step that worker is currently doing (or . if they are idle). The Done column
+shows completed steps.
+
+Note that the order of the steps has changed; this is because steps now take
+time to finish and multiple workers can begin multiple steps simultaneously.
+
+In this example, it would take 15 seconds for two workers to complete these
+steps.
+
+With 5 workers and the 60+ second step durations described above, how long will
+it take to complete all of the steps?
+
+*/
+	private static final Pattern INPUT_PATTERN = Pattern.compile(
+			"Step (\\w) must be finished before step (\\w) can begin.");
+
+	private List<Character> get(Map<Character, List<Character>> map, Character key) {
+		return map.computeIfAbsent(key, k -> new ArrayList<>());
+	}
+
+	private Map<Character, List<Character>> parseInput(List<String> lines) {
+		Map<Character, List<Character>> result = new TreeMap<>();
+		for (String line : lines) {
+			Matcher matcher = INPUT_PATTERN.matcher(line);
+			if (!matcher.matches()) throw new IllegalStateException("Bad line: " + line);
+			Character req  = matcher.group(1).charAt(0);
+			Character from = matcher.group(2).charAt(0);
+			// Add the requirement
+			get(result, from).add(req);
+			// Make sure the required letter is also present in the map, even if
+			// it has no requirements itself
+			get(result, req);
+		}
+		return result;
+	}
+
+	// Pops the next fulfilled preReq
+	private Character popNext(
+			Map<Character, List<Character>> preReqs,
+			Set<Character> done) {
+		Character best = null;
+		for (Character key : preReqs.keySet()) {
+			List<Character> keyPreReqs = preReqs.get(key);
+			if (done.containsAll(keyPreReqs)) {
+				best = key;
+				break;
+			}
+		}
+		if (best == null) return null;
+		preReqs.remove(best);
+		return best;
+	}
+
+	public String evalPart1(List<String> lines) {
+		Map<Character, List<Character>> preReqs = parseInput(lines);
+		Set<Character> done   = new HashSet<>();
+		StringBuilder  answer = new StringBuilder();
+
+		while (!preReqs.isEmpty()) {
+			Character next = popNext(preReqs, done);
+			if (next == null) throw new IllegalStateException("Cannot solve");
+			answer.append(next);
+			done.add(next);
+		}
+		return answer.toString();
+	}
+
+	class Worker {
+		final Character job;
+		int             endTick;
+
+		Worker(Character job, int endTick) {
+			this.job     = job;
+			this.endTick = endTick;
+		}
+	}
+
+	public int evalPart2(List<String> lines, int numWorkers, int extraTime) {
+		Map<Character, List<Character>> preReqs = parseInput(lines);
+		Set<Character> done = new HashSet<>();
+
+		List<Worker> workers = new ArrayList<>();
+		int tick = 0;
+		while (!preReqs.isEmpty() || !workers.isEmpty()) {
+			// Assign a job to each available worker
+			while (workers.size() < numWorkers) {
+				Character next = popNext(preReqs, done);
+				// Is there a job ready?
+				if (next == null) break;
+				// Start it
+				workers.add(new Worker(
+						next,
+						tick + extraTime + next - 'A' + 1));
+			}
+			// Work on jobs
+			if (workers.isEmpty()) throw new IllegalStateException("No jobs");
+			tick++;
+			for (Iterator<Worker> it = workers.iterator(); it.hasNext(); ) {
+				Worker worker = it.next();
+				// Check for finished job
+				if (worker.endTick <= tick) {
+					done.add(worker.job);
+					it.remove();
+				}
+			}
+		}
+		return tick;
+	}
+
+	public static void main(String[] args) throws Exception {
+		Day7 solver = new Day7();
+		List<String> input = ResourceUtil.readAllLines("2018/day7.input");
+
+		// Part 1
+		// Example
+		List<String> example = ImmutableList.of(
+				"Step C must be finished before step A can begin.",
+				"Step C must be finished before step F can begin.",
+				"Step A must be finished before step B can begin.",
+				"Step A must be finished before step D can begin.",
+				"Step B must be finished before step E can begin.",
+				"Step D must be finished before step E can begin.",
+				"Step F must be finished before step E can begin."
+		);
+		Test.check(solver.evalPart1(example), "CABDFE");
+		// Vs Input
+		System.out.println(solver.evalPart1(input));
+
+		// Part 2
+		// Example
+		Test.check(solver.evalPart2(example, 2, 0), 15);
+		// Vs Input
+		System.out.println(solver.evalPart2(input, 5, 60));
+	}
+
+}
---
+++ src/main/java/aoc/_2018/Day9.java
@@ -0,0 +1,241 @@
+package aoc._2018;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.collect.ImmutableMap;
+import shared.ResourceUtil;
+import shared.Test;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Dan Fielding
+ */
+public class Day9 {
+/*
+--- Day 9: Marble Mania ---
+
+You talk to the Elves while you wait for your navigation system to initialize.
+To pass the time, they introduce you to their favorite marble game.
+
+The Elves play this game by taking turns arranging the marbles in a circle
+according to very particular rules. The marbles are numbered starting with 0 and
+increasing by 1 until every marble has a number.
+
+First, the marble numbered 0 is placed in the circle. At this point, while it
+contains only a single marble, it is still a circle: the marble is both
+clockwise from itself and counter-clockwise from itself. This marble is
+designated the current marble.
+
+Then, each Elf takes a turn placing the lowest-numbered remaining marble into
+the circle between the marbles that are 1 and 2 marbles clockwise of the current
+marble. (When the circle is large enough, this means that there is one marble
+between the marble that was just placed and the current marble.) The marble that
+was just placed then becomes the current marble.
+
+However, if the marble that is about to be placed has a number which is a
+multiple of 23, something entirely different happens. First, the current player
+keeps the marble they would have placed, adding it to their score. In addition,
+the marble 7 marbles counter-clockwise from the current marble is removed from
+the circle and also added to the current player's score. The marble located
+immediately clockwise of the marble that was removed becomes the new current
+marble.
+
+For example, suppose there are 9 players. After the marble with value 0 is
+placed in the middle, each player (shown in square brackets) takes a turn. The
+result of each of those turns would produce circles of marbles like this, where
+clockwise is to the right and the resulting current marble is in parentheses:
+
+[-] (0)
+[1]  0 (1)
+[2]  0 (2) 1
+[3]  0  2  1 (3)
+[4]  0 (4) 2  1  3
+[5]  0  4  2 (5) 1  3
+[6]  0  4  2  5  1 (6) 3
+[7]  0  4  2  5  1  6  3 (7)
+[8]  0 (8) 4  2  5  1  6  3  7
+[9]  0  8  4 (9) 2  5  1  6  3  7
+[1]  0  8  4  9  2(10) 5  1  6  3  7
+[2]  0  8  4  9  2 10  5(11) 1  6  3  7
+[3]  0  8  4  9  2 10  5 11  1(12) 6  3  7
+[4]  0  8  4  9  2 10  5 11  1 12  6(13) 3  7
+[5]  0  8  4  9  2 10  5 11  1 12  6 13  3(14) 7
+[6]  0  8  4  9  2 10  5 11  1 12  6 13  3 14  7(15)
+[7]  0(16) 8  4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[8]  0 16  8(17) 4  9  2 10  5 11  1 12  6 13  3 14  7 15
+[9]  0 16  8 17  4(18) 9  2 10  5 11  1 12  6 13  3 14  7 15
+[1]  0 16  8 17  4 18  9(19) 2 10  5 11  1 12  6 13  3 14  7 15
+[2]  0 16  8 17  4 18  9 19  2(20)10  5 11  1 12  6 13  3 14  7 15
+[3]  0 16  8 17  4 18  9 19  2 20 10(21) 5 11  1 12  6 13  3 14  7 15
+[4]  0 16  8 17  4 18  9 19  2 20 10 21  5(22)11  1 12  6 13  3 14  7 15
+[5]  0 16  8 17  4 18(19) 2 20 10 21  5 22 11  1 12  6 13  3 14  7 15
+[6]  0 16  8 17  4 18 19  2(24)20 10 21  5 22 11  1 12  6 13  3 14  7 15
+[7]  0 16  8 17  4 18 19  2 24 20(25)10 21  5 22 11  1 12  6 13  3 14  7 15
+
+The goal is to be the player with the highest score after the last marble is
+used up. Assuming the example above ends after the marble numbered 25, the
+winning score is 23+9=32 (because player 5 kept marble 23 and removed marble 9,
+while no other player got any points in this very short example game).
+
+Here are a few more examples:
+
+    10 players; last marble is worth 1618 points: high score is 8317
+    13 players; last marble is worth 7999 points: high score is 146373
+    17 players; last marble is worth 1104 points: high score is 2764
+    21 players; last marble is worth 6111 points: high score is 54718
+    30 players; last marble is worth 5807 points: high score is 37305
+
+What is the winning Elf's score?
+
+--- Part Two ---
+
+Amused by the speed of your answer, the Elves are curious:
+
+What would the new winning Elf's score be if the number of the last marble were
+100 times larger?
+
+ */
+
+	private static final Pattern INPUT_PATTERN = Pattern.compile(
+			"(\\d+) players; last marble is worth (\\d+) points");
+
+	// DIY linked list...
+	class Marble {
+		final int value;
+		Marble    prev;
+		Marble    next;
+
+		Marble(int value) {
+			this.value = value;
+			this.prev  = this;
+			this.next  = this;
+		}
+
+		@Override
+		public String toString() {
+			return MoreObjects.toStringHelper(this)
+					.add("value", value)
+					.add("prev", prev.value)
+					.add("next", next.value)
+					.toString();
+		}
+
+		String debug() {
+			StringBuilder result = new StringBuilder();
+			result.append(value);
+			Marble next = this.next;
+			while (next != this) {
+				result.append(" ");
+				result.append(next.value);
+				next = next.next;
+			}
+			return result.toString();
+		}
+	}
+
+	// Step back or forward X jumps
+	private Marble step(Marble current, int steps) {
+		while (steps > 0) {
+			current = current.next;
+			steps--;
+		}
+		while (steps < 0) {
+			current = current.prev;
+			steps++;
+		}
+		return current;
+	}
+
+	// Insert after current, returns inserted marble
+	private Marble insert(Marble current, Marble toInsert) {
+		Marble next   = current.next;
+		current.next  = toInsert;
+		next.prev     = toInsert;
+
+		toInsert.next = next;
+		toInsert.prev = current;
+		return toInsert;
+	}
+
+	// Removes current, returns the marble after the one removed
+	private Marble remove(Marble current) {
+		current.next.prev = current.prev;
+		current.prev.next = current.next;
+		// Shift to next
+		current = current.next;
+		return current;
+	}
+
+	public long eval(int numPlayers, int lastMarble) {
+		long[] scores = new long[numPlayers];
+
+		Marble zero    = new Marble(0);
+		Marble current = zero;
+		int    currentPlayer = 0;
+		int    nextNewMarble = 1;
+		while (nextNewMarble <= lastMarble) {
+			current = step(current, 1);
+
+			Marble marble = new Marble(nextNewMarble);
+			// Multiples of 23 are scorers
+			boolean scoringMarble = (marble.value % 23 == 0);
+			if (!scoringMarble) {
+				current = insert(current, marble);
+			} else {
+				current = step(current, -8);
+				Marble removed = current;
+				current = remove(removed);
+				scores[currentPlayer] += marble.value + removed.value;
+			}
+
+			// Next turn
+			currentPlayer = (currentPlayer + 1) % numPlayers;
+			nextNewMarble++;
+		}
+		return Arrays.stream(scores)
+				.max()
+				.orElseThrow(() -> new IllegalStateException("No max"));
+	}
+
+	public long evalInput(String input, int lastMarbleFactor) {
+		Matcher matcher = INPUT_PATTERN.matcher(input);
+		if (!matcher.matches()) throw new IllegalStateException("Bum input: " + input);
+		int numPlayers = Integer.parseInt(matcher.group(1));
+		int lastMarble = Integer.parseInt(matcher.group(2)) * lastMarbleFactor;
+		return eval(numPlayers, lastMarble);
+	}
+
+	public long evalPart1(String input) {
+		return evalInput(input, 1);
+	}
+
+	public long evalPart2(String input) {
+		return evalInput(input, 100);
+	}
+
+	public static void main(String[] args) throws Exception {
+		Day9   solver = new Day9();
+		String input  = ResourceUtil.readString("2018/day9.input");
+		Map<String, Long> examples = ImmutableMap.<String, Long>builder()
+				.put( "9 players; last marble is worth 32 points",   32L)
+				.put("10 players; last marble is worth 1618 points", 8317L)
+				.put("13 players; last marble is worth 7999 points", 146373L)
+				.put("17 players; last marble is worth 1104 points", 2764L)
+				.put("21 players; last marble is worth 6111 points", 54718L)
+				.put("30 players; last marble is worth 5807 points", 37305L)
+				.build();
+
+		// Part 1
+		// Examples
+		for (String key : examples.keySet()) {
+			Test.check(solver.evalPart1(key), examples.get(key));
+		}
+		// Vs Input
+		System.out.println(solver.evalPart1(input));
+
+		// Part 2
+		System.out.println(solver.evalPart2(input));
+	}
+}
